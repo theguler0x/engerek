4faa77cbc979924a89de9412cfd91f1dc4507387
Arda Nural <arda.nural@biznet.com.tr>
2015-12-15 17:46:07 +0200

    page login update

--- gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/login/PageLogin.java
+++ gui/admin-gui/src/main/java/com/evolveum/midpoint/web/page/login/PageLogin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2013 Evolveum
+ * Copyright (c) 2010-2015 Evolveum
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,51 +16,37 @@
 
 package com.evolveum.midpoint.web.page.login;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.rmi.server.UID;
-
-import javax.imageio.ImageIO;
-import javax.servlet.http.Cookie;
-
 import com.evolveum.midpoint.schema.result.OperationResult;
 import com.evolveum.midpoint.security.api.AuthorizationConstants;
 import com.evolveum.midpoint.task.api.Task;
 import com.evolveum.midpoint.util.exception.ObjectNotFoundException;
 import com.evolveum.midpoint.util.exception.SchemaException;
-import com.evolveum.midpoint.web.application.AuthorizationAction;
+import com.evolveum.midpoint.util.logging.Trace;
+import com.evolveum.midpoint.util.logging.TraceManager;
 import com.evolveum.midpoint.web.application.PageDescriptor;
-import com.evolveum.midpoint.web.component.menu.top.LocalePanel;
-import com.evolveum.midpoint.web.component.menu.top.TopMenuBar;
 import com.evolveum.midpoint.web.page.PageBase;
 import com.evolveum.midpoint.web.page.admin.home.PageDashboard;
 import com.evolveum.midpoint.web.page.forgetpassword.PageForgetPassword;
+import com.evolveum.midpoint.web.page.self.PageSelfDashboard;
 import com.evolveum.midpoint.web.page.selfregistration.PageSelfRegistration;
+import com.evolveum.midpoint.web.security.MidPointApplication;
 import com.evolveum.midpoint.web.security.MidPointAuthWebSession;
+import com.evolveum.midpoint.web.security.SecurityUtils;
+import com.evolveum.midpoint.web.util.WebMiscUtil;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.CredentialsPolicyType;
-import com.octo.captcha.service.multitype.GenericManageableCaptchaService;
-import com.octo.captcha.service.*;
-import com.octo.captcha.service.*;
 
-import org.apache.wicket.Session;
-import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.RequiredTextField;
-import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
-import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.html.panel.FeedbackPanel;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.extensions.markup.html.captcha.CaptchaImageResource;
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.request.http.WebResponse;
-import org.apache.wicket.request.resource.DynamicImageResource;
-import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.apache.wicket.util.time.Time;
-import org.apache.wicket.validation.IValidatable;
-import org.apache.wicket.validation.validator.AbstractValidator;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.wicket.util.value.ValueMap;
+import org.apache.wicket.markup.html.image.Image;
+import org.apache.wicket.markup.html.image.NonCachingImage;
+import org.apache.wicket.markup.html.link.Link;
 
 /**
  * @author mserbak
@@ -68,155 +54,126 @@ import org.springframework.beans.factory.annotation.Autowired;
 @PageDescriptor(url = "/login")
 public class PageLogin extends PageBase {
 
-	PageBase page = getPageBase();
-	private static final String ID_LOGIN_FORM = "loginForm";
+	private static final Trace LOGGER = TraceManager.getTrace(PageLogin.class);
+	
+    PageBase page = getPageBase();
+    private static final String ID_LOGIN_FORM = "loginForm";
 
-	private static final String ID_USERNAME = "username";
-	private static final String ID_PASSWORD = "password";
+    private static final String ID_USERNAME = "username";
+    private static final String ID_PASSWORD = "password";
+    private static final String ID_CAPTCHA = "captchatextfield";
+    private final CaptchaImageResource captchaImageResource;
 
-	protected static final String OPERATION_LOAD_RESET_PASSWORD_POLICY = "LOAD PASSWORD RESET POLICY";
-	@SpringBean(name = "captchaService")
-	private static CaptchaServiceSingleton captchaService;
-	private String challengeId = null;
-	private String challengeResponse;
+    protected static final String OPERATION_LOAD_RESET_PASSWORD_POLICY = "LOAD PASSWORD RESET POLICY";
 
-	// private static final ImageCaptchaService captchaService = new
-	// DefaultManageableImageCaptchaService();
+    
+    public PageLogin() {
+        if (SecurityUtils.getPrincipalUser() != null) {
+            MidPointApplication app = getMidpointApplication();
+            setResponsePage(app.getHomePage());
+        }
 
-	public PageLogin() {
-		TopMenuBar menuBar = getTopMenuBar();
-		menuBar.addOrReplace(new LocalePanel(TopMenuBar.ID_RIGHT_PANEL));
+        Form form = new Form(ID_LOGIN_FORM) {
+        	
+        	
+            @Override
+            protected void onSubmit() {
+                MidPointAuthWebSession session = MidPointAuthWebSession.getSession();
+                
+                if (!imagePass.equals(getPassword()))
+                {
+                    //error("Captcha şifresi '" + getPassword() + "' yanlış.\n" +"Doğru şifre: " + imagePass);
+                    error("Karakter doğrulama yanlış!");
+					// force redrawing
+                    return;
+                }
+                else
+                {
+                    info("Başarılı!");
+                    // force redrawing                   
+                }
+				captchaImageResource.invalidate();
+               
+                RequiredTextField<String> username = (RequiredTextField) get(ID_USERNAME);
+                PasswordTextField password = (PasswordTextField) get(ID_PASSWORD);
+                if (session.authenticate(username.getModelObject(), password.getModelObject())) {
+                    if (WebMiscUtil.isAuthorized(AuthorizationConstants.AUTZ_UI_DASHBOARD_URL,
+                            AuthorizationConstants.AUTZ_UI_HOME_ALL_URL)) {
+                        setResponsePage(PageDashboard.class);
+                    } else {
+                        setResponsePage(PageSelfDashboard.class);
+                    }
+                }
+            }
+        };
+        OperationResult parentResult = new OperationResult(OPERATION_LOAD_RESET_PASSWORD_POLICY);
 
-		Form form = new Form(ID_LOGIN_FORM) {
+        CredentialsPolicyType creds = null;
+        try {
+            creds = getModelInteractionService().getCredentialsPolicy(null, (Task) null, parentResult);
+        } catch (ObjectNotFoundException | SchemaException e) {
+            LOGGER.warn("Cannot read credentials policy: "+e.getMessage(), e);
+        }
+        BookmarkablePageLink<String> link = new BookmarkablePageLink<String>("forgetpassword", PageForgetPassword.class);
+        boolean linkIsVisible = false;
+        if (creds != null && creds.getSecurityQuestions() != null && creds.getSecurityQuestions().getQuestionNumber() != null) {
+            linkIsVisible = true;
+        }
+        link.setVisible(linkIsVisible);
+        form.add(link);
 
-			
-			@Override
-			protected void onSubmit() {
-				MidPointAuthWebSession session = MidPointAuthWebSession
-						.getSession();
+        form.add(new RequiredTextField(ID_USERNAME, new Model<String>()));
+        form.add(new PasswordTextField(ID_PASSWORD, new Model<String>()));
+        
+        captchaImageResource = new CaptchaImageResource(imagePass);
+		Image image = new NonCachingImage("captchaImage",captchaImageResource);
+		form.add(image);
+    	//form.add(new Image("captchaImage", captchaImageResource));
+    	form.add(new RequiredTextField<String>(ID_CAPTCHA, new PropertyModel<String>(properties,
+    			ID_CAPTCHA))
+            {
+                @Override
+                protected final void onComponentTag(final ComponentTag tag)
+                {
+                    super.onComponentTag(tag);
+                    // clear the field after each render
+                    tag.put("value", "");
+                }
+            });
+        form.add(new Link("btnselfregistration"){
+            @Override
+            public void onClick(){
+                setResponsePage(PageSelfRegistration.class);
+            }
+        });
 
-				RequiredTextField<String> username = (RequiredTextField) get(ID_USERNAME);
-				PasswordTextField password = (PasswordTextField) get(ID_PASSWORD);
-				 info(getLocalizer().getString("captcha.validation.succeeded",
-				          this));
-				if (session.authenticate(username.getModelObject(),
-						password.getModelObject())) {
-					captchaService.getService().emptyCaptchaStore();
-					setResponsePage(PageDashboard.class);
+        add(form);
+    }
+    
+    private static int randomInt(int min, int max)
+    {
+        return (int)(Math.random() * (max - min) + min);
+    }
+    
+    private static String randomString(int min, int max)
+    {
+        int num = randomInt(min, max);
+        byte b[] = new byte[num];
+        for (int i = 0; i < num; i++)
+            b[i] = (byte)randomInt('a', 'z');
+        return new String(b);
+    }
+    
+    /** Random captcha password to match against. */
+    private final String imagePass = randomString(6, 8);
+    private final ValueMap properties = new ValueMap();
+    
+    private String getPassword()
+    {
+        return properties.getString(ID_CAPTCHA);
+    }
 
-
-				}
-			}
-
-			
-		};
-
-		OperationResult parentResult = new OperationResult(
-				OPERATION_LOAD_RESET_PASSWORD_POLICY);
-
-		try {
-			CredentialsPolicyType creds = getModelInteractionService()
-					.getCredentialsPolicy(null, parentResult);
-			BookmarkablePageLink<String> link = new BookmarkablePageLink<String>(
-					"forgetpassword", PageForgetPassword.class);
-
-			boolean linkIsVisible = false;
-			if (creds != null) {
-				if (creds.getSecurityQuestions().getQuestionNumber() != null) {
-					linkIsVisible = true;
-
-				}
-
-			}
-			link.setVisible(linkIsVisible);
-			form.add(link);
-
-		} catch (ObjectNotFoundException | SchemaException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		add(new Link("selfregistration"){
-			@Override
-			public void onClick(){
-					setResponsePage(PageSelfRegistration.class);
-			}
-		});
-
-
-
-		DynamicImageResource res = new DynamicImageResource() {
-			
-		
-			
-			@Override
-			protected byte[] getImageData(Attributes attributes) {
-				ByteArrayOutputStream os = new ByteArrayOutputStream();
-				challengeId = new UID().toString();
-				BufferedImage challenge = captchaService.getService().
-						getImageChallengeForID(challengeId, Session.get().getLocale());
-
-				try {
-					
-		            ImageIO.write(challenge, "jpeg", os);
-		            return os.toByteArray();
-		          } catch (Exception e) {
-		            throw new RuntimeException(e);
-		          }
-				
-			}
-			
-		};
-
-		form.add(new RequiredTextField(ID_USERNAME, new Model<String>()));
-		form.add(new PasswordTextField(ID_PASSWORD, new Model<String>()));
-		form.add(new Image("captchaImage",res));
-		form.add(new RequiredTextField("response", new PropertyModel(this,
-		          "challengeResponse")) {
-			@Override
-			  protected final void onComponentTag(final ComponentTag tag) {
-		          super.onComponentTag(tag);
-		          tag.put("value", "");
-		        }
-		}.add(new AbstractValidator() {
-
-			@Override
-			protected void onValidate(IValidatable validatable) {
-				if (!captchaService.getService().validateResponseForID(challengeId,
-			              validatable.getValue())) {
-			            error(validatable);
-			          }				
-				}
-			 @Override
-		        protected String resourceKey() {
-		          return "captcha.validation.failed";
-		        }
-		}));
-			
-		form.add(new FeedbackPanel("feedback"));
-		form.add(new Link("btnselfregistration"){
-			@Override
-			public void onClick(){
-				setResponsePage(PageSelfRegistration.class);
-			}
-		});
-
-		add(form);
-	}
-	public String getChallengeResponse() {
-	      return challengeResponse;
-	    }
-
-	    public void setChallengeResponse(String challengeResponse) {
-	      this.challengeResponse = challengeResponse;
-	    }
-
-	/*
-	 * @Override protected IModel<String> createPageTitleModel() { return new
-	 * Model<>(""); }
-	 */
-
-	public PageBase getPageBase() {
-		return (PageBase) getPage();
-	}
+    public PageBase getPageBase() {
+        return (PageBase) getPage();
+    }
 }
